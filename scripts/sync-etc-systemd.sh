b91e2a343e3475a8144ec4321fc0818339170694
Aetf <aetf@unlimitedcodeworks.xyz>
2015-09-08 00:32:10 -0400

    Sync only systemd units

--- scripts/sync-etc-systemd.sh
+++ scripts/sync-etc-systemd.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Syncing /home/aetf/customizations/etc/systemd/system to /etc/systemd/system"
-cp /home/aetf/customizations/etc/systemd/system/* /etc/systemd/system
+cp /home/aetf/customizations/etc/systemd/system/*.{service,socket,device,mount,automount,swap,target,path,timer,snapshot,slice,scope} /etc/systemd/system
 
 #sleep a second before reload
 sleep 1
