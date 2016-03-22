878ee50fd7cc08ec6fd69cfe0739b15a766182f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 16:00:49 -0400

    Use default tmux for Clarity machines

--- home/config/fish/functions/ssh.fish
+++ home/config/fish/functions/ssh.fish
@@ -4,7 +4,7 @@ function ssh --description 'Login to vps with tmux'
             case 'vps'
                 command ssh $argv 'tmux new -As \'Arch\''
             case 'clarity*'
-                command ssh $argv 'bash'
+                command ssh $argv 'tmux new -As \'Peifeng\''
         end
     else
         command ssh $argv
