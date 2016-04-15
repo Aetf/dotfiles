7abc54b1a95744f9b2f5a5d2333f74e5573124a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 16:27:33 -0400

    use exec to start tmux

--- home/config/fish/functions/ssh.fish
+++ home/config/fish/functions/ssh.fish
@@ -2,9 +2,9 @@ function ssh --description 'Login to vps with tmux'
     if test (count $argv) -ge 1
         switch $argv[1]
             case 'vps'
-                command ssh $argv 'tmux new -As \'Arch\''
+                command ssh $argv 'exec tmux new -As Arch'
             case 'clarity*'
-                command ssh $argv 'tmux new -As \'Peifeng\''
+                command ssh $argv 'exec tmux new -As Peifeng'
         end
     else
         command ssh $argv
