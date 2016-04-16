35522caddd66133eac01deefed5615593c7f2e62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 22:30:15 -0400

    Improve ssh.fish args handling

--- home/config/fish/functions/ssh.fish
+++ home/config/fish/functions/ssh.fish
@@ -1,6 +1,6 @@
 function ssh --description 'Login to vps with tmux' -w ssh
-    if test (count $argv) -eq 1
-        switch $argv[1]
+    if test (count $argv) -ge 1
+        switch $argv[-1]
             case 'vps'
                 command ssh $argv 'exec tmux new -As Arch'
             case 'clarity*'
