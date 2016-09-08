94f703971e7e04d8217027dd3c85b8f962c0a622
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 14:50:37 -0400

    Fix venv creation

--- home/config/fish/functions/venv.fish
+++ home/config/fish/functions/venv.fish
@@ -5,17 +5,20 @@ function venv --description 'Create python virtualenv in ~/.local/venvs'
     else
         set venv_path $argv[-1]
         set -e argv[-1]
-        if not test -f $venv_path/bin/activate.fish
-            set venv_path ~/.local/venvs/$argv[-1]
+        if not test -f "$venv_path/bin/activate.fish"
+            set venv_path ~/.local/venvs/$venv_path
         end
-        if test -f $venv_path/bin/activate.fish
-            #fish -c ". $venv_path/bin/activate.fish; exec fish"
-            # hacky workaround for virtualenv nonsense cleanup
-            env _OLD_VIRTUAL_PATH= _OLD_VIRTUAL_PYTHONHOME= _OLD_FISH_PROMPT_OVERRIDE= fish -c ". $venv_path/bin/activate.fish; exec fish"
-        else
-            if confirm "Virtualenv $venv_path not found. create?"
+
+        if not test -f "$venv_path/bin/activate.fish"
+            if confirm "Virtualenv $venv_path not found. Create?"
                 virtualenv $argv $venv_path
+            else
+                return
             end
         end
+
+        #fish -c ". $venv_path/bin/activate.fish; exec fish"
+        # hacky workaround for virtualenv nonsense cleanup
+        env _OLD_VIRTUAL_PATH= _OLD_VIRTUAL_PYTHONHOME= _OLD_FISH_PROMPT_OVERRIDE= fish -c ". $venv_path/bin/activate.fish; exec fish"
     end
 end
