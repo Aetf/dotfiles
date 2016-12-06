6bfdaa6b6f00e38c8a11d02dd64c66d38de53c40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 18:47:49 -0500

    fish: fix working in kmscon

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -5,19 +5,24 @@
 # If not running interactively, don't do anything
 if not status -i; exit; end
 
-# Some environment variables
-## More colorful
-if test $TERM != "screen-256color" -a $TERM != "linux";
-    set TERM "konsole"
-    # explicitly enable 256color in fish
-    set fish_term256 1
+# If ssh-agent haven't been started, we are in kmscon or linux console
+# start ssh-agent with tmux
+if test "$SSH_AUTH_SOCK"x = "x";
+    exec ssh-agent tmux new -As console
 end
 
+# Some environment variables
 ## Only set these if we don't have DISPLAY
 ## otherwise, these've been set in $HOME/.xprofile
-if test "$DISPLAYx" = "x";
+if test "$DISPLAY"x = "x";
+    ## Locale
+    set -x LANG zh_CN.UTF8
+    set -x LC_ALL zh_CN.UTF-8
+    ## Install directory
+    set -x INSTALLDIR "$HOME/software"
     ## Default editor
     set -x EDITOR vim
+    set -x VISUAL $EDITOR
     ## Used for virtualbox
     set -x VBOX_USB usbfs
     ## Nasm environment
@@ -32,13 +37,17 @@ if test "$DISPLAYx" = "x";
     set -x XDG_CONFIG_HOME "$HOME/.config"
 
     # Prepend user's bin directory to PATH
-    set PATH ~/bin $PATH
+    if test -d "$HOME/customizations/scripts"
+        set PATH "$HOME/customizations/scripts" $PATH
+    end
     ## more in INSTALLDIR
-    for dir in (find $INSTALLDIR -maxdepth 1 -mindepth 1 -type d -not -name src -print);
-        begin
-            set PATH $PATH "$dir";
-            if [ -d "$dir/bin" ];
-                set PATH $PATH "$dir/bin";
+    if test -d "$INSTALLDIR"
+        for dir in "$INSTALLDIR/*"
+            if test -d "$dir" -a (basename $dir) != "src"
+                set PATH "$PATH" "$dir"
+                if test -d "$dir/bin"
+                    set PATH $PATH "$dir/bin"
+                end
             end
         end
     end
@@ -46,23 +55,15 @@ if test "$DISPLAYx" = "x";
     for p in ~/.gem/ruby/*/bin
         set PATH $PATH $p
     end
+    ## Node.js executable
+    if test -d "$HOME/.node_modules/bin"
+        set PATH $PATH "$HOME/.node_modules/bin"
+    end
+    ## Local bin directory
+    if test -d "$HOME/.local/bin"
+        set PATH $PATH "$HOME/.local/bin"
+    end
 end
-
-# Make time output more informations
-set -x TIME
-set TIME "\
-Time\n\
-\tTotal:\t\t%es\n\
-\tIn user:\t%Us\n\
-\tIn kernel:\t%Ss\n\
-\tCPU perentage:\t%P\n\
-Memory\n\
-\tUsage:\t\t%Xk+%Dk / %Kk (Shared+Unshared / Total)\n\
-\tPage faults:\t%F/%R (Major/Minor)\n\
-\tSwapped out:\t%W times\n\
-I/O\n\
-\tInputs:\t\t%I\n\
-\tOutputs:\t%O"
 
 # Some useful alias (functions in .config/fish/functions)
 # ll='ls -lh'
@@ -75,14 +76,27 @@ I/O\n\
 # work='cd /tmp/workspace'
 # grepc='grep --color=always'
 
+## More colorful
+switch $TERM
+    case 'xterm' 'konsole'
+        set TERM konsole
+        # explicitly enable 256color and 24bit color in fish
+        set fish_term256 1
+        set fish_term24bit 1
+    case '*256col*'
+        # should not need this, as fish will enable 256 color if the term contains "256color"
+        #set fish_term256 1
+        set fish_term24bit 1
+    case 'tmux'
+        set fish_term256 1
+        set fish_term24bit 1
+end
+
 # Prompt line hook provided by powerline
 switch $TERM
-    case 'xterm*' 'konsole*'
+    case 'xterm*' 'konsole' 'tmux'
         powerline-setup
     case 'linux'
-        set -x POWERLINE_CONFIG_OVERRIDES 'common.term_truecolor=false'
-        powerline-setup
-    case 'screen-256color'
         set -x POWERLINE_CONFIG_OVERRIDES 'common.term_truecolor=false'
         powerline-setup
     case '*'
@@ -91,16 +105,9 @@ switch $TERM
         end
 end
 
-# Silence 'Picked up _JAVA_OPTIONS' message on command line
+# Silence 'Picked up _JAVA_OPTIONS' message on command line (combined with java.fish)
 set _SILENT_JAVA_OPTIONS "$_JAVA_OPTIONS"
 set -e _JAVA_OPTIONS
-
-
-# Start ssh-agent if we havn't done so
-if test "$SSH_AUTH_SOCK"x = "x";
-    exec ssh-agent fish
-end
-
 
 # Default command for fzf
 set -x FZF_DEFAULT_COMMAND 'plocate "^"(pwd)'
