3ac83ea997ffad4a3038806e07b0f494047ca208
Aetf <aetf@unlimitedcodeworks.xyz>
2017-11-02 22:12:51 -0400

    ssh: use RequestTTY only when open shell

--- home/config/fish/functions/ssh.fish
+++ home/config/fish/functions/ssh.fish
@@ -1,12 +1,12 @@
 function ssh --description 'Login to vps with tmux' -w ssh
     if test (count $argv) -ge 1
         switch $argv[-1]
-            case 'vps'
-                command ssh $argv 'exec tmux new -As Arch'
+            case '*vps'
+                command ssh -t $argv 'exec tmux new -As Arch'
             case 'cc*'
-                command ssh $argv 'exec tmux new -As Peifeng'
+                command ssh -t $argv 'exec tmux new -As Peifeng'
             case 'clarity*'
-                command ssh $argv 'exec tmux new -As Peifeng'
+                command ssh -t $argv 'exec tmux new -As Peifeng'
             case '*'
                 command ssh $argv
         end
