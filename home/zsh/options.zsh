5c1c8769792a3d198be21d4208d3ad7bfceb634f
Aetf <aetf@unlimitedcodeworks.xyz>
2018-07-12 16:11:51 -0400

    zsh: add fuzzy completion

--- home/zsh/options.zsh
+++ home/zsh/options.zsh
@@ -22,3 +22,14 @@ setopt COMBINING_CHARS
 #compinit -d
 #zstyle ':completion:*' menu select
 #setopt COMPLETE_ALIASES
+#
+
+# Fuzzy tab completion
+# 0 -- vanilla completion (abc => abc)
+# 1 -- smart case completion (abc => Abc)
+# 2 -- word flex completion (abc => A-big-Car)
+# 3 -- full flex completion (abc => ABraCadabra)
+zstyle ':completion:*' matcher-list '' \
+  'm:{a-z\-}={A-Z\_}' \
+  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+  'r:|?=** m:{a-z\-}={A-Z\_}'
