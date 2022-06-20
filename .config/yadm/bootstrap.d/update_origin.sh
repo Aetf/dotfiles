720b857dd21c9933ea24aca6ddddbd7583fe892c
Aetf <aetf@unlimited-code.works>
2022-06-20 01:10:33 -0700

    Initial with bootstrap

---
+++ .config/yadm/bootstrap.d/update_origin.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+() {
+    local origin=$(yadm remote get-url origin)
+    local old="https://github.com/"
+    local new="git@github.com:"
+    origin="${origin/#$old/$new}"
+    echo "Updating the yadm repo origin URL to $origin"
+    yadm remote set-url origin "$origin"
+}
+
