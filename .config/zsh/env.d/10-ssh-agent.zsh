17fb27b4eace0a369f822a76af0aa3516d3cefb5
Peifeng Yu <pfyu@google.com>
2022-07-25 14:37:53 -0700

    zsh: skip set SSH_AUTH_SOCK if ssh-agent isn't started by systemd

--- .config/zsh/env.d/10-ssh-agent.zsh
+++ .config/zsh/env.d/10-ssh-agent.zsh
@@ -1,2 +1,6 @@
 # SSH-agent is managed by systemd --user scope
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+() {
+    if [[ -S "$XDG_RUNTIME_DIR/ssh-agent.socket" ]]; then
+        export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+    fi
+}
