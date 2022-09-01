3e328be27dc0bb8d8dd2142c6a69a964583b6f81
Aetf <aetf@unlimited-code.works>
2022-09-01 00:50:45 -0700

    zerotier: note about moon

--- .config/aconfmgr/roles/zerotier.bash
+++ .config/aconfmgr/roles/zerotier.bash
@@ -1,4 +1,6 @@
 ## MANUAL: join zerotier-one network using `sudo zerotier-cli join <network-ID>`
+## MANUAL: join moon: sudo zerotier-cli orbit c3755c24d1
 AddPackage zerotier-one # Creates virtual Ethernet networks of almost unlimited size.
 SystemdEnable zerotier-one /usr/lib/systemd/system/zerotier-one.service
+
 
