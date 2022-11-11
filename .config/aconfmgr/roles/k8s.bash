4c24faf9f90f16844a484f1445a925a737fbf35b
Aetf <aetf@unlimited-code.works>
2022-11-10 23:13:50 -0800

    aconfmgr: k8s: ignore more state

--- .config/aconfmgr/roles/k8s.bash
+++ .config/aconfmgr/roles/k8s.bash
@@ -1,5 +1,11 @@
 AddPackage $FOREIGN k3s-1.24-bin
 SystemdEnable k3s-1.24-bin /usr/lib/systemd/system/k3s.service
 
+# Node feature isn't configuration
+IgnorePath /etc/kubernetes/node-feature-discovery/*
+
 # Tokens are private
 IgnorePath /etc/rancher/k3s/*.token
+
+# Juicefs volume plugin isn't isolated and will create this
+IgnorePath /etc/updatedb.conf
