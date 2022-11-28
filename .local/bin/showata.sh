7ce9c2f8608b6fb50571843b903a51b55aa19e8b
Aetf <aetf@unlimited-code.works>
2022-11-27 22:59:23 -0800

    showata.sh: egrep is deprecated

--- .local/bin/showata.sh
+++ .local/bin/showata.sh
@@ -9,10 +9,10 @@ fi
 for x in /sys/block/sd*
 do
     dev=$(basename $x)
-    host=$(ls -l $x | egrep -o "host[0-9]+")
-    target=$(ls -l $x | egrep -o "target[0-9:]*")
+    host=$(ls -l $x | grep -E -o "host[0-9]+")
+    target=$(ls -l $x | grep -E -o "target[0-9:]*")
     a=$(cat /sys/class/scsi_host/$host/unique_id)
-    a2=$(echo $target | egrep -o "[0-9]:[0-9]$" | sed 's/://')
+    a2=$(echo $target | grep -E -o "[0-9]:[0-9]$" | sed 's/://')
     serial=$(hdparm -I /dev/$dev | grep "Serial Number" | sed 's/^[ \t]*//')
     echo -e "$dev \t ata$a.$a2 \t $serial"
 done
