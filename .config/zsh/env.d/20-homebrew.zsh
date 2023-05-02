8033b7453723736a0e59c0c0e3fc3a15f6d213ce
Peifeng Yu <pfyu@google.com>
2023-05-02 15:25:06 -0700

    homebrew: stop using brewed curl and git which are likely to break

--- .config/zsh/env.d/20-homebrew.zsh
+++ .config/zsh/env.d/20-homebrew.zsh
@@ -2,8 +2,8 @@
 () {
     if [ -d $HOME/.linuxbrew ]; then
         eval "$($HOME/.linuxbrew/bin/brew shellenv)"
-        export HOMEBREW_FORCE_BREWED_CURL=1
-        export HOMEBREW_FORCE_BREWED_GIT=1
+        #export HOMEBREW_FORCE_BREWED_CURL=1
+        #export HOMEBREW_FORCE_BREWED_GIT=1
     fi
     if [ -d /opt/homebrew ]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
