2c4a87d5d9e143f0058bf967d58d00d8dc91b5ba
Aetf <aetf@unlimited-code.works>
2023-05-14 14:23:24 -0700

    rust: rust-script is now a community package

--- .config/aconfmgr/roles/rust-dev.bash
+++ .config/aconfmgr/roles/rust-dev.bash
@@ -1,5 +1,5 @@
 
-AddPackage $FOREIGN rust-script-git # Run Rust files and expressions without any setup or compilation necessary
+AddPackage rust-script # Run Rust files and expressions without any setup or compilation necessary
 AddPackage rustup # The Rust toolchain installer
 AddPackage $FOREIGN cargo-criterion # Cargo plugin for analyzing and reporting on Criterion-rs benchmarks.
 AddPackage cargo-edit # Managing cargo dependencies from the command line
