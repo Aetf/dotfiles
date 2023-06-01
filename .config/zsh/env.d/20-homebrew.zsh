33f3bd92f95410c80c7734d57c760f197a70440e
Peifeng Yu <pfyu@google.com>
2023-06-01 00:02:29 +0000

    homebrew: disable analysis

--- .config/zsh/env.d/20-homebrew.zsh
+++ .config/zsh/env.d/20-homebrew.zsh
@@ -4,6 +4,7 @@
         eval "$($HOME/.linuxbrew/bin/brew shellenv)"
         #export HOMEBREW_FORCE_BREWED_CURL=1
         #export HOMEBREW_FORCE_BREWED_GIT=1
+        export HOMEBREW_NO_ANALYTICS=1
     fi
     if [ -d /opt/homebrew ]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
