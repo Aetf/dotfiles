a34e77b447f8245ccc92b37ecf844ccc660d3a50
Peifeng Yu <pfyu@google.com>
2023-06-01 00:32:43 +0000

    zsh: update homebrew to check more paths

--- .config/zsh/env.d/20-homebrew.zsh
+++ .config/zsh/env.d/20-homebrew.zsh
@@ -6,6 +6,12 @@
         #export HOMEBREW_FORCE_BREWED_GIT=1
         export HOMEBREW_NO_ANALYTICS=1
     fi
+    if [ -d /home/linuxbrew/.linuxbrew ]; then
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        #export HOMEBREW_FORCE_BREWED_CURL=1
+        #export HOMEBREW_FORCE_BREWED_GIT=1
+        export HOMEBREW_NO_ANALYTICS=1
+    fi
     if [ -d /opt/homebrew ]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
         export HOMEBREW_NO_ANALYTICS=1
