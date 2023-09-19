83e9c3ea509ca9379f598e6be357800ffb0706f4
Aetf <aetf@unlimited-code.works>
2023-09-18 23:49:38 -0700

    ssh: no need to customize /etc/ssh/sshd_config
    Arch package supports /etc/ssh/sshd_config.d directly now.
    
    fixes #2

--- .config/aconfmgr/roles/ssh.bash
+++ .config/aconfmgr/roles/ssh.bash
@@ -1,12 +1,6 @@
 
 AddPackage openssh # Premier connectivity tool for remote login with the SSH protocol
 
-# Create an infrastructure to use individual sshd config files
-CreateDir /etc/ssh/sshd_config.d
-cat >> "$(GetPackageOriginalFile openssh /etc/ssh/sshd_config)" <<EOF
-Include sshd_config.d/*.conf
-EOF
-
 # Allow local autorized keys in addition to distributed ones
 cat > "$(CreateFile /etc/ssh/sshd_config.d/local-authorized-keys.conf)" <<EOF
 AuthorizedKeysFile .ssh/authorized_keys .ssh/authorized_keys.local
