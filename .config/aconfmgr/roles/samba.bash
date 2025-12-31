18d13d19d5fce896bf15303debd05167bcc015d9
Aetf <aetf@unlimited-code.works>
2025-12-30 18:59:45 -0800

    nas: switch to userspace samba
    ksmbd is more unreliable and some clients have weird behavior when
    interacting with it (e.g. Solid Explorer on Android).

--- .config/aconfmgr/roles/samba.bash
+++ .config/aconfmgr/roles/samba.bash
@@ -1,19 +1,33 @@
 # Use kernel space ksmbd server
 
-AddPackage $FOREIGN ksmbd-tools # Userspace tools for the ksmbd kernel SMB server
-SystemdEnable ksmbd-tools /usr/lib/systemd/system/ksmbd.service
+#AddPackage $FOREIGN ksmbd-tools # Userspace tools for the ksmbd kernel SMB server
+#SystemdEnable ksmbd-tools /usr/lib/systemd/system/ksmbd.service
 
-cat >$(CreateFile /etc/ksmbd/ksmbd.conf) <<EOF
-[global]
-    deadtime = 10
-[nas]
-    path = /mnt/nas
-    read only = no
-    force user = aetf
-    force group = aetf
-EOF
+# cat >$(CreateFile /etc/ksmbd/ksmbd.conf) <<EOF
+# [global]
+#     deadtime = 10
+# [nas]
+#     path = /mnt/nas
+#     read only = no
+#     force user = aetf
+#     force group = aetf
+# EOF
 
-# Need create ksmbd user manually afterwards
+# Need to create ksmbd user manually afterwards
 # MANUAL: ksmbd.adduser -a aetf
 IgnorePath '/etc/ksmbd/ksmbdpwd.db'
 IgnorePath '/etc/ksmbd/ksmbd.subauth'
+
+# Usual samba package
+AddPackage samba # SMB Fileserver and AD Domain server
+CopyFile /etc/samba/smb.conf
+SystemdEnable samba /usr/lib/systemd/system/smb.service
+
+# Enable mDNS discovery of the service, depending on enabling of systemd-resolved
+CopyFile /etc/systemd/dnssd/smb.dnssd
+
+# Need to create samba user manually afterwards
+# MANUAL: smbpasswd -a aetf
+#IgnorePath '/etc/ksmbd/ksmbdpwd.db'
+#IgnorePath '/etc/ksmbd/ksmbd.subauth'
+
